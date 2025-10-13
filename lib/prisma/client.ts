3c0c734b1dd8ee72093472894f9a2ab0a86627a1
Olivier RUBUZ <rubuzolivier2@gmail.com>
2025-10-13 04:50:56 +0200

    Fixed the PrismaClient import error!

--- lib/prisma/client.ts
+++ lib/prisma/client.ts
@@ -7,7 +7,7 @@ export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: ['error', 'warn'],
-  })
+  }) 
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
